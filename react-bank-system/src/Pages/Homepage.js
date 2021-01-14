7974d70480e4ccee1b92b04b203c4fbec574b7e5
Kushagra-Madhukar <73365229+Kushagra-Madhukar@users.noreply.github.com>
2021-01-14 19:26:08 +0530

    Added responsiveness, fixed ui bugs

--- react-bank-system/src/Pages/Homepage.js
+++ react-bank-system/src/Pages/Homepage.js
@@ -7,16 +7,16 @@ const Homepage = () => {
        
     return (
         <>
-        <main>
+        
             <div className="hm-text-container">
-                <h1>Supervillain Vault</h1>
+                <h1 className="hm-text-head">Supervillain Vault</h1>
                 <p>Are you a supervillain? Have you gotten tired of keeping in check of your finances while fighting with the superheroes? If that's the case then we have news for you.</p>
                 <p>"Supervillain vault" is a non profit organisation dedicated to the cause of providing monetary protection to the hard working supervillains of our universe. We provide you with quick money transfer access and a community support of a large number of supervillains who will have your back when your pockets run out of cash. We are trusted by the pioneers in the supervillain community, "Thanos", "Joker", "Ultron" to name a few.</p>
                 <p>We also provide funding to new and creative ideas of world dominance/destruction put forward by our fellow members. We are empathetic to the cause of all the supervillains to make the world a better barren place. With your dedication and our support we will succeed one day. </p>
                 <p>Our tech team is continuously working on improving the user experience and new updates will be coming in the near future. You can try a demo by clicking on the button below to see how our platform works. </p>
                 <Link to = "/customers"><button className="hm-main-btn">View Customers</button></Link>
             </div>
-        </main>
+        
         </>
 
     )
